5ceaf22838969fe1d48479211b396905a45f8f1b
CarolesHaddad <carolhvs0912@gmail.com>
2025-04-23 20:50:18 -0300

    asas

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="877105616701.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t aws-crud .
+docker build -t aws-crud -f app/Dockerfile
 docker tag aws-crud:latest $ECR_REGISTRY/aws-crud:latest
 docker push $ECR_REGISTRY/aws-crud:latest
